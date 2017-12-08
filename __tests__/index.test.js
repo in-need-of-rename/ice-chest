9cd12dff0e7403e3d4c5cf33a4e811d3edd570e1
Gary Borton <gdborton@gmail.com>
2017-12-08 00:14:02 -0800

    Make entry point a class that can be instantiated.
    The entry point now points to a class that can be instantiated to
    prevent passing the same options around everywhere.

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,69 +1,32 @@
 const fs = require('fs');
 const path = require('path');
-const zlib = require('zlib');
 
 const rimraf = require('rimraf');
-const mkdirp = require('mkdirp');
-const iceChest = require('../index');
+const IceChest = require('../index');
 
-const cacheLocation = path.resolve(path.join(__dirname, './.cache'));
+jest.mock('../lib/low-level');
+const lowIceChest = require('../lib/low-level');
+const cacheLocation = path.resolve(path.join(__dirname, '.cache'));
 
 beforeEach(() => {
   rimraf.sync(cacheLocation);
 });
 
-test('createCacheKey returns a string for an object.', () => {
-  expect(typeof iceChest.createCacheKey({ test: 1})).toBe('string');
+test('creating and instance of IceChest should create the cacheLocation', () => {
+  const iceChest = new IceChest({ cacheLocation });
+  expect(() => {
+    fs.statSync(cacheLocation);
+  }).not.toThrow();
 });
 
-test('createCacheKey on two identical objects should return the same hash', () => {
-  const hash1 = iceChest.createCacheKey({ test: 1 });
-  const hash2 = iceChest.createCacheKey({ test: 1 });
-  expect(hash1).toBe(hash2);
-});
-
-test('createCacheKey should return different hashes for two different objects', () => {
-  const hash1 = iceChest.createCacheKey({ test: 1 });
-  const hash2 = iceChest.createCacheKey({ test: 2 });
-  expect(hash1).not.toBe(hash2);
-});
-
-test('writeToCache returns a promise', () => {
-  const obj = { test: 1 };
-  const cacheKey = iceChest.createCacheKey(obj);
-  const result = iceChest.writeToCache(cacheKey, obj, cacheLocation);
-  expect(result instanceof Promise).toBeTruthy();
-  return result;
-});
-
-test('writeToCache writes JSON serialized data to the cache directory', () => {
-  const obj = { test: 1 };
-  const cacheKey = iceChest.createCacheKey(obj);
-  return iceChest.writeToCache(cacheKey, obj, cacheLocation).then(() => {
-    const fileLocation = path.join(cacheLocation, `${cacheKey}.json`);
-    const writtenObject = JSON.parse(fs.readFileSync(fileLocation));
-    // deep equal, different refernce
-    expect(writtenObject).toEqual(obj);
-  });
-});
-
-test('loadFromCache returns the JSON parsed object stored there', () => {
-  const obj = { test: 1 };
-  const fakeCacheKey = 'abcd';
-  mkdirp.sync(cacheLocation);
-  fs.writeFileSync(path.join(cacheLocation, `${fakeCacheKey}.json`), JSON.stringify(obj));
-  return iceChest.loadFromCache(fakeCacheKey, cacheLocation).then((readObject) => {
-    expect(readObject).toEqual(obj);
-  });
-});
-
-test('loadFromCache support gzipped files', () => {
-  const obj = { test: 1 };
-  const fakeCacheKey = 'abcd';
-  mkdirp.sync(cacheLocation);
-  const gzippedBuffer = zlib.gzipSync(JSON.stringify(obj));
-  fs.writeFileSync(path.join(cacheLocation, `${fakeCacheKey}.json.gz`), gzippedBuffer);
-  return iceChest.loadFromCache(fakeCacheKey, cacheLocation).then((readObject) => {
-    expect(readObject).toEqual(obj);
-  });
+test('iceChest instances wrap low-level functions', () => {
+  const obj = { test: 1};
+  const fakeCacheKey = 'asdf';
+  const iceChest = new IceChest({ cacheLocation });
+  iceChest.createCacheKey(obj);
+  expect(lowIceChest.createCacheKey).toBeCalledWith(obj);
+  iceChest.writeToCache(fakeCacheKey, obj);
+  expect(lowIceChest.writeToCache).toBeCalledWith(fakeCacheKey, obj, cacheLocation);
+  iceChest.loadFromCache(fakeCacheKey);
+  expect(lowIceChest.loadFromCache).toBeCalledWith(fakeCacheKey, cacheLocation);
 });
